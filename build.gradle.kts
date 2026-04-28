482d8eff61778ec095e0a89ad8e923374f227924
wonnho71 <wonnho@nate.com>
2026-04-28 11:08:50 +0900

    arange gradle project; test connection

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,9 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation ("org.mariadb.jdbc:mariadb-java-client:3.3.3")
+    compileOnly("javax.servlet:javax.servlet-api:4.0.1")
 }
+
 
 tasks.test {
     useJUnitPlatform()
--- src/test/java/org/workbook/dao/ConnectTest.java
+++ src/test/java/org/workbook/dao/ConnectTest.java
@@ -1,4 +1,14 @@
 package org.workbook.dao;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class ConnectTest {
+
+    @Test
+    public void test() {
+        int v1=131;
+        int v2=131;
+        Assertions.assertEquals(v1,v2);
+    }
 }
