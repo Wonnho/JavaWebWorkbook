a520b0ae55fa2913e064b77efc56cffee415b6af
wonnho71 <wonnho@nate.com>
2026-05-02 00:12:30 +0900

    rewrite web.xml so as to operate annotation under webapp/WEB-INf/ and relocate resources/log4j2.xml under main for log information at terminal, the location of which is vital

---
+++ src/main/java/org/workbook/controller/TodoRegisterController.java
@@ -0,0 +1,67 @@
+package org.workbook.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.workbook.dto.TodoDTO;
+import org.workbook.service.TodoService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@WebServlet(name="todoRegisterController", value="/todo/register")
+@Log4j2
+public class TodoRegisterController extends HttpServlet {
+
+    private TodoService todoService = TodoService.INSTANCE;
+    private final DateTimeFormatter DATAFORMATTER =
+            DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+        log.info("/todo/register GET ..... ");
+
+        HttpSession session = req.getSession();
+
+        if (session.isNew()) {
+            log.info("JSESSIONID cookie is newly created");
+            res.sendRedirect("/login");
+            return;
+        }
+
+        if (session.getAttribute("loginInfo") == null) {
+            log.info("user having no login information");
+            res.sendRedirect("/login");
+            return;
+        }
+
+        req.getRequestDispatcher("/WEB-INF/todo/register.jsp").forward(req, res);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+
+        TodoDTO todoDTO = TodoDTO.builder()
+                .title(req.getParameter("title"))
+                .dueDate(LocalDate.parse(req.getParameter("dueDate"), DATAFORMATTER))
+                .build();
+        log.info("/todo/register POST....");
+        log.info(todoDTO);
+        try {
+            todoService.register(todoDTO);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        res.sendRedirect("/todo/list");
+    }
+}
--- src/main/webapp/WEB-INF/todo/register.jsp
+++ src/main/webapp/WEB-INF/todo/register.jsp
@@ -7,6 +7,16 @@
 
 <body>
 <form action="/todo/register" method="post" >
+<div>
+<input type="text" name="title" placeholder="INSERT TITLE">
+</div>
+
+<div>
+<input type="date" name="dueDate">
+</div>
+
+<div>
+<button type="reset">RESET</button>
 <button type="submit">Register</button>
 </form>
 
