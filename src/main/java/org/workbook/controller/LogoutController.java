882515817e3120910b58b39e7dbae388e16ae25c
wonnho71 <wonnho@nate.com>
2026-05-02 15:21:17 +0900

    write logoutController and add form of logout at list.jsp

---
+++ src/main/java/org/workbook/controller/LogoutController.java
@@ -0,0 +1,27 @@
+package org.workbook.controller;
+
+import lombok.extern.log4j.Log4j2;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/logout")
+@Log4j2
+public class LogoutController extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        log.info("log out ..........");
+        HttpSession session=request.getSession();
+        session.removeAttribute("loginInfo");
+        session.invalidate();
+
+        response.sendRedirect("/");
+    }
+}
--- src/main/webapp/WEB-INF/todo/list.jsp
+++ src/main/webapp/WEB-INF/todo/list.jsp
@@ -20,5 +20,9 @@
     </c:forEach>
 </ul>
 
+<form action="/logout" method="post">
+<button>LOGOUT</button>
+</form>
+
 </body>
 </html>
