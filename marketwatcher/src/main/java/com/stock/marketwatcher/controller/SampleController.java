ccfcfdaca99c52139f3b82ffd3a3b7ee4c77aac7
wonnho71 <wonnho@nate.com>
2026-05-17 02:49:48 +0900

    status, th:if,th:unless,? ternary operator,th:switch~th:case

--- marketwatcher/src/main/java/com/stock/marketwatcher/controller/SampleController.java
+++ marketwatcher/src/main/java/com/stock/marketwatcher/controller/SampleController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 @Log4j2
 public class SampleController {
@@ -13,5 +16,17 @@ public class SampleController {
     public void springboot(Model model) {
         log.info("Hey Guys, Welcome to Spring Boot World.........................");
         model.addAttribute("message","Hey Guys, Welcome to Spring Boot World");
+    }
+
+    @GetMapping("/thymeleaf/jackson")
+    public void jackson(Model model) {
+        List<String> list=Arrays.asList("JSON","javascript on notation","jackson","databind");
+        model.addAttribute("List",list);
+    }
+
+    @GetMapping("/thymeleaf/repetition")
+    public void repetition(Model model) {
+        List<String> list=Arrays.asList("JSON","<th:each>","<ch:block>","repetition");
+        model.addAttribute("List",list);
     }
 }
