9ebc021f6dfdbb5c790bfa911e755d2a404e452d
wonnho71 <wonnho@nate.com>
2026-05-19 20:56:38 +0900

    create BaseEntity denoting date, time indicator

---
+++ marketwatcher/src/main/java/com/stock/marketwatcher/domain/BaseEntity.java
@@ -0,0 +1,29 @@
+package com.stock.marketwatcher.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(value={AuditingEntityListener.class})
+@Getter
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(name="regdate",updatable = false)
+    private LocalDate regDate;
+
+    @LastModifiedDate
+    @Column(name="moddate")
+    private LocalDateTime modDate;
+
+}
