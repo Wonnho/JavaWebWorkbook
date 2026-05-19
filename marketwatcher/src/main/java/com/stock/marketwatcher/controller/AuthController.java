f943976f7c50127dbaa315ed9c8cdfb02ccb0044
wonnho71 <wonnho@nate.com>
2026-05-19 20:01:57 +0900

    write Board class under domain

--- marketwatcher/src/main/java/com/stock/marketwatcher/controller/AuthController.java
+++ marketwatcher/src/main/java/com/stock/marketwatcher/controller/AuthController.java
@@ -1,4 +1,16 @@
 package com.stock.marketwatcher.controller;
 
+import com.stock.marketwatcher.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
 public class AuthController {
+    private final User user;
+
+    @GetMapping("/signup")
+    public void signup() {
+
+
+    }
 }
--- marketwatcher/src/main/java/com/stock/marketwatcher/domain/Board.java
+++ marketwatcher/src/main/java/com/stock/marketwatcher/domain/Board.java
@@ -1,5 +1,21 @@
 package com.stock.marketwatcher.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity
 public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bno;
+    private String title;
+
+    private String content;
+
+    private String writer;
 
 }
