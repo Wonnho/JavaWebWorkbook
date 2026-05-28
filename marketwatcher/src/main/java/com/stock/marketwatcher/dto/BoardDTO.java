104594030c34afef4a78bf7fc93094a240210dd5
wonnho71 <wonnho@nate.com>
2026-05-28 17:34:45 +0900

    create BoardDTO und dto made

--- marketwatcher/src/main/java/com/stock/marketwatcher/dto/BoardDTO.java
+++ marketwatcher/src/main/java/com/stock/marketwatcher/dto/BoardDTO.java
@@ -1,4 +1,19 @@
 package com.stock.marketwatcher.dto;
 
+import java.time.LocalDateTime;
+
 public class BoardDTO {
+
+    private Long bno;
+
+    private String title;
+
+    private String content;
+
+    private String writer;
+
+    private LocalDateTime regDate;
+
+    private LocalDateTime modDate;
+
 }
