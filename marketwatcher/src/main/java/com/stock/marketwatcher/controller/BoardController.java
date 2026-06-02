d142f582c6c4dc41788d12b25433fb7b9dae706c
wonnho71 <wonnho@nate.com>
2026-06-02 13:46:31 +0900

    write listing presentation with date format

--- marketwatcher/src/main/java/com/stock/marketwatcher/controller/BoardController.java
+++ marketwatcher/src/main/java/com/stock/marketwatcher/controller/BoardController.java
@@ -1,6 +1,9 @@
 package com.stock.marketwatcher.controller;
 
+import com.stock.marketwatcher.domain.Board;
+import com.stock.marketwatcher.dto.BoardDTO;
 import com.stock.marketwatcher.dto.PageRequestDTO;
+import com.stock.marketwatcher.dto.PageResponseDTO;
 import com.stock.marketwatcher.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -19,5 +22,8 @@ public class BoardController {
     @GetMapping("/list")
     public void list(PageRequestDTO pageRequestDTO, Model model) {
 
+       PageResponseDTO<BoardDTO> responseDTO=boardService.list(pageRequestDTO);
+    log.info(responseDTO);
+    model.addAttribute("responseDTO",responseDTO);
     }
 }
