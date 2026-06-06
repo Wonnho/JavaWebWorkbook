03bc685301c227bd45384f16c4e1cad96d2e578b
wonnho71 <wonnho@nate.com>
2026-06-07 00:43:11 +0900

    testSearchReplyCount()

--- marketwatcher/src/test/java/com/stock/marketwatcher/repository/BoardRepositoryTests.java
+++ marketwatcher/src/test/java/com/stock/marketwatcher/repository/BoardRepositoryTests.java
@@ -1,6 +1,7 @@
 package com.stock.marketwatcher.repository;
 
 import com.stock.marketwatcher.domain.Board;
+import com.stock.marketwatcher.dto.BoardListReplyCountDTO;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -116,5 +117,27 @@ public class BoardRepositoryTests {
         result.getContent().forEach(board -> log.info(board));
     }
 
+    @Test
+    public void testSearchReplyCount() {
+
+        String[] types = {"t","c","w"};
+
+        String keyword = "1";
+
+        Pageable pageable = PageRequest.of(0,10, Sort.by("bno").descending());
+
+        Page<BoardListReplyCountDTO> result = boardRepository.searchWithReplyCount(types, keyword, pageable );
+
+        //total pages
+        log.info(result.getTotalPages());
+        //pag size
+        log.info(result.getSize());
+        //pageNumber
+        log.info(result.getNumber());
+        //prev next
+        log.info(result.hasPrevious() +": " + result.hasNext());
+
+        result.getContent().forEach(board -> log.info(board));
+    }
 
 }
