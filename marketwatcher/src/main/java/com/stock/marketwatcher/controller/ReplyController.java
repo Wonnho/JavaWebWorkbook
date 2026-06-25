8ec4756eab40e786d1bbb06e819c2df5ed3c7990
wonnho71 <wonnho@nate.com>
2026-06-25 21:28:59 +0900

    reply register method and test it at Swagger-UI and check the reply update at Heidi

--- marketwatcher/src/main/java/com/stock/marketwatcher/controller/ReplyController.java
+++ marketwatcher/src/main/java/com/stock/marketwatcher/controller/ReplyController.java
@@ -1,8 +1,10 @@
 package com.stock.marketwatcher.controller;
 
 import com.stock.marketwatcher.dto.ReplyDTO;
+import com.stock.marketwatcher.service.ReplyService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -19,7 +21,10 @@ import java.util.Map;
 @RestController
 @Log4j2
 @RequestMapping("/replies")
+@RequiredArgsConstructor
 public class ReplyController {
+
+    private final ReplyService replyService;
 
     @Operation(summary="Replies POST",description="make a comment as POST method")
     @PostMapping(value="/",consumes = MediaType.APPLICATION_JSON_VALUE)
@@ -32,6 +37,7 @@ public class ReplyController {
         }
 
      Map<String,Long>   resultMap=new HashMap<>();
+        Long rno=replyService.register(replyDTO);
         resultMap.put("rno",111L);
 
         return resultMap;
