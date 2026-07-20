277077cac845e8c207a2163541304ebd083fcc64
wonnho71 <wonnho@nate.com>
2026-07-20 12:48:35 +0900

    update reply.js and read.html so deal with reply asyncrously by dint of axios

--- marketwatcher/src/main/resources/static/js/reply.js
+++ marketwatcher/src/main/resources/static/js/reply.js
@@ -1,4 +1,10 @@
 async function get1(bno) {
  const result=await axios.get(`/replies/list/${bno}`)
- console.log(result)
+ //console.log(result)
+return result.data
+}
+
+async function getList({bno,page,size,goList}) {
+  const result=await axios.get(`/replies/list/${bno}`, {params:{page,size}})
+  return result.data
 }
